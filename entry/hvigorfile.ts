8c0d297b7a393dbd0630817324d21cfac291a558
张轩铭 <2300990296@qq.com>
2025-03-23 09:30:08 +0800

    测试

---
+++ entry/hvigorfile.ts
@@ -0,0 +1,4 @@
+import { hapTasks } from '@ohos/hvigor-ohos-plugin'
+export default {
+    system: hapTasks
+}
---
+++ hvigorfile.ts
@@ -0,0 +1,4 @@
+import { appTasks } from '@ohos/hvigor-ohos-plugin'
+export default {
+    system: appTasks
+}
